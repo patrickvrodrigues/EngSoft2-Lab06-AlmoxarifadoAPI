502ff455a2c5d7d6d763b7239e6a0dd09a584ca6
Patrick Viegas <patrickviegas4@gmail.com>
2020-12-20 01:21:21 -0300

    Adicionado modelo Solicitante, Builder para testes e os Testes de classe

---
+++ src/test/java/almoxarifado/modelo/SolicitanteModeloTest.java
@@ -0,0 +1,28 @@
+package almoxarifado.modelo;
+
+import almoxarifado.modelo.builder.SolicitanteBuilder;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class SolicitanteModeloTest {
+    @Test
+    public void deveCriarSolicitanteComBuilder(){
+        Solicitante solicitante = SolicitanteBuilder.umSolicitante().constroi();
+
+        Assert.assertEquals("Patrick Viegas", solicitante.getNome());
+        Assert.assertEquals("99999999999", solicitante.getCpf());
+        Assert.assertEquals("9899999999", solicitante.getTelefone());
+    }
+
+    @Test
+    public void deveCriarSolicitanteCostumizadoComBuilder(){
+        Solicitante solicitante = SolicitanteBuilder.umSolicitante()
+                .comNome("Fernando")
+                .comCpf("1234")
+                .comTelefone("8888").constroi();
+        Assert.assertEquals("Fernando", solicitante.getNome());
+        Assert.assertEquals("1234", solicitante.getCpf());
+        Assert.assertEquals("8888",solicitante.getTelefone());
+
+    }
+}
---
+++ src/test/java/almoxarifado/modelo/builder/SolicitanteBuilder.java
@@ -0,0 +1,37 @@
+package almoxarifado.modelo.builder;
+
+import almoxarifado.modelo.Solicitante;
+
+public class SolicitanteBuilder {
+    private Solicitante solicitante;
+
+    private SolicitanteBuilder(){}
+
+    public static SolicitanteBuilder umSolicitante(){
+        SolicitanteBuilder builder = new SolicitanteBuilder();
+
+        builder.solicitante = new Solicitante();
+
+        builder.solicitante.setNome("Patrick Viegas");
+        builder.solicitante.setCpf("99999999999");
+        builder.solicitante.setTelefone("9899999999");
+        return  builder;
+    }
+
+    public SolicitanteBuilder comNome(String nome){
+        this.solicitante.setNome(nome);
+        return this;
+    }
+    public SolicitanteBuilder comCpf(String cpf){
+        this.solicitante.setCpf(cpf);
+        return this;
+    }
+    public SolicitanteBuilder comTelefone(String telefone){
+        this.solicitante.setTelefone(telefone);
+        return this;
+    }
+
+    public Solicitante constroi(){
+        return this.solicitante;
+    }
+}
