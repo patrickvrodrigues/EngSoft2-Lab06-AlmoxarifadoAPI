23372535ead650c7226707feaf532a7ec2dde9e4
Patrick Viegas <patrickviegas4@gmail.com>
2020-12-20 12:37:49 -0300

    consertando config para teste

--- src/test/java/almoxarifado/teste/modelo/SolicitanteModeloTest.java
+++ src/test/java/almoxarifado/teste/modelo/SolicitanteModeloTest.java
@@ -1,17 +1,16 @@
 package almoxarifado.teste.modelo;
 
 import almoxarifado.builder.SolicitanteBuilder;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SolicitanteModeloTest {
     @Test
     public void deveCriarSolicitanteComBuilder(){
         Solicitante solicitante = SolicitanteBuilder.umSolicitante().constroi();
-
-        Assert.assertEquals("Patrick Viegas", solicitante.getNome());
-        Assert.assertEquals("99999999999", solicitante.getCpf());
-        Assert.assertEquals("9899999999", solicitante.getTelefone());
+        Assertions.assertEquals("Patrick Viegas", solicitante.getNome());
+        Assertions.assertEquals("99999999999", solicitante.getCpf());
+        Assertions.assertEquals("9899999999", solicitante.getTelefone());
     }
 
     @Test
@@ -20,9 +19,9 @@ public class SolicitanteModeloTest {
                 .comNome("Fernando")
                 .comCpf("1234")
                 .comTelefone("8888").constroi();
-        Assert.assertEquals("Fernando", solicitante.getNome());
-        Assert.assertEquals("1234", solicitante.getCpf());
-        Assert.assertEquals("8888",solicitante.getTelefone());
+        Assertions.assertEquals("Fernando", solicitante.getNome());
+        Assertions.assertEquals("1234", solicitante.getCpf());
+        Assertions.assertEquals("8888",solicitante.getTelefone());
 
     }
 }
